503a07ee10ae75899945dd70bab8d479a8c2cb8e
ridwanalfarezi <donnysutiandy@gmail.com>
2024-07-20 11:42:35 +0700

    add: next-auth

---
+++ next-auth/src/app/settings/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import getSession from "@/lib/getSession";
+import prisma from "@/lib/prisma";
+import { UpdateProfileValues, updateProfileSchema } from "@/lib/validation";
+import { revalidatePath } from "next/cache";
+
+// To learn more about server actions, watch my YT tutorial: https://www.youtube.com/watch?v=XD5FpbVpWzk
+
+export async function updateProfile(values: UpdateProfileValues) {
+  const session = await getSession();
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const { name } = updateProfileSchema.parse(values);
+
+  await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      name,
+    },
+  });
+
+  revalidatePath("/");
+}
---
+++ next-auth/src/auth.ts
@@ -0,0 +1,28 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Resend from "next-auth/providers/resend";
+import prisma from "./lib/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
+  theme: {
+    logo: "/logo.png",
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role;
+      return session;
+    },
+  },
+  providers: [
+    Google,
+    Github,
+    Resend({
+      from: "no-reply@ridwanalfarezi.is-a.dev",
+    }),
+  ],
+});
